06ffdd9b697723d40318fe0213d08ed000873597
leon004 <lcalderonsast@gmail.com>
2024-05-16 00:21:44 -0600

    Admin

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,42 +11,31 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   private createAuthorizationHeader(): HttpHeaders {
-    const token = localStorage.getItem('userToken');
+    const token = localStorage.getItem('token');
     return new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     });
   }
 
-  // Obtener todos los usuarios
   getAllUsers(): Observable<any> {
     return this.http.get(this.baseUrl, {
       headers: this.createAuthorizationHeader()
     });
   }
 
-  // Obtener un usuario por ID
-  getUserById(id: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${id}`, {
-      headers: this.createAuthorizationHeader()
-    });
-  }
-
-  // Crear un nuevo usuario
   createUser(user: any): Observable<any> {
     return this.http.post(this.baseUrl, user, {
       headers: this.createAuthorizationHeader()
     });
   }
 
-  // Actualizar un usuario por ID
   updateUser(id: number, user: any): Observable<any> {
     return this.http.put(`${this.baseUrl}/${id}`, user, {
       headers: this.createAuthorizationHeader()
     });
   }
 
-  // Eliminar un usuario por ID
   deleteUser(id: number): Observable<any> {
     return this.http.delete(`${this.baseUrl}/${id}`, {
       headers: this.createAuthorizationHeader()
