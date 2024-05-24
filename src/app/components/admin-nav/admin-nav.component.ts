1f440fbe767f950de29ef8708d09b90986294c35
leon004 <l20120965@morelia.tecnm.mx>
2024-05-24 09:34:53 -0600

    fixing bugs in admin-nav component

--- src/app/components/admin-nav/admin-nav.component.ts
+++ src/app/components/admin-nav/admin-nav.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';  // Importar correctamente desde @angular/router
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-nav',
   templateUrl: './admin-nav.component.html',
-  styleUrls: ['./admin-nav.component.css']  // Corregir 'styleUrls' en lugar de 'styleUrl'
+  styleUrls: ['./admin-nav.component.css']
 })
 export class AdminNavComponent implements OnInit {
 
   userRole: string | null = null;
+  homeRoute: string = '/home'; // Ruta por defecto
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.userRole = localStorage.getItem('rol');
+    this.setHomeRoute();
+  }
+
+  setHomeRoute(): void {
+    if (this.userRole === 'Admin') {
+      this.homeRoute = '/admin-dashboard';
+    } else if (this.userRole === 'Policia') {
+      this.homeRoute = '/police-dashboard';
+    } else if (this.userRole === 'Juez') {
+      this.homeRoute = '/judge-dashboard';
+    } else {
+      this.homeRoute = '/default-dashboard';
+    }
   }
 }
